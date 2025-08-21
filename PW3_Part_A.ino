74d5512ede2c11932efc162afde52918a2f16b31
kingdiaw <kingdiawehsut@gmail.com>
2025-08-21 11:57:29 +0800

    Update PW3_Part_A.ino
    Add debungging message

--- PW3_Part_A.ino
+++ PW3_Part_A.ino
@@ -11,9 +11,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   int btn_state = digitalRead(btnPin);
   if(btn_state == HIGH){
+    Serial.println("RLY ACTIVATE");
     pinMode(relayPin, OUTPUT);
     digitalWrite(relayPin, LOW);
+    delay(1000);
   }else{
+    Serial.println("RLY DEACTIVATE");
     pinMode(relayPin,INPUT);
+    delay(1000);
   }
 }
